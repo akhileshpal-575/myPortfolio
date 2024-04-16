51353acf9e391d8eaaa20a9f1817c00228dd86d8
Akhilesh Pal <palakhilesh466@gmail.com>
2024-04-16 21:56:34 +0530

    update in responsiveness

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,30 +16,31 @@ import mysql from '../assets/MySQL-Dark.svg'
 const Skills = () => {
  
   return (
-    <section id="skills" className="py-10 bg-gray-800 relative">
-      <div className="mt-8 text-gray-100 text-center">
-        <h3 className="text-4xl font-semibold">
-          My <span className="text-cyan-600">Skills</span>
-        </h3>
-        <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
-        <div className="flex items-center w-[80%] justify-center mt-12 ml-32 gap-10 flex-wrap">
-               
-                <img className="w-20 h-20  cursor-pointer" src={c}/>
-                <img className="w-20 h-20  cursor-pointer" src={cpp}/>
-                <img className="w-20 h-20  cursor-pointer" src={java}/>
-                <img className="w-20 h-20  cursor-pointer" src={python}/>
-                <img className="w-20 h-20  cursor-pointer" src={html}/>
-                <img className="w-20 h-20  cursor-pointer" src={css}/>
-                <img className="w-20 h-20  cursor-pointer" src={tailwind}/>
-                <img className="w-20 h-20  cursor-pointer" src={js}/>
-                <img className="w-20 h-20  cursor-pointer" src={react1}/>
-                <img className="w-20 h-20  cursor-pointer" src={nodejs}/>
-                <img className="w-20 h-20  cursor-pointer" src={expressjs}/>
-                <img className="w-20 h-20  cursor-pointer" src={mongodb}/>
-                <img className="w-20 h-20  cursor-pointer" src={mysql}/>
-        </div>
-        </div>
-    </section>
+    <section id="skills" className="py-10 bg-gray-800 w-full relative">
+  <div className="mt-8 text-gray-100 text-center">
+    <h3 className="text-4xl font-semibold">
+      My <span className="text-cyan-600">Skills</span>
+    </h3>
+    <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
+    <div className="flex items-center justify-center mt-12 mx-28 gap-12 flex-wrap">
+      {/* Example images */}
+      <img className="w-20 h-20 cursor-pointer" src={c} alt="C" />
+      <img className="w-20 h-20 cursor-pointer" src={cpp} alt="C++" />
+      <img className="w-20 h-20 cursor-pointer" src={java} alt="Java" />
+      <img className="w-20 h-20 cursor-pointer" src={python} alt="Python" />
+      <img className="w-20 h-20 cursor-pointer" src={html} alt="HTML" />
+      <img className="w-20 h-20 cursor-pointer" src={css} alt="CSS" />
+      <img className="w-20 h-20 cursor-pointer" src={tailwind} alt="Tailwind" />
+      <img className="w-20 h-20 cursor-pointer" src={js} alt="JavaScript" />
+      <img className="w-20 h-20 cursor-pointer" src={react1} alt="React" />
+      <img className="w-20 h-20 cursor-pointer" src={nodejs} alt="Node.js" />
+      <img className="w-20 h-20 cursor-pointer" src={expressjs} alt="Express.js" />
+      <img className="w-20 h-20 cursor-pointer" src={mongodb} alt="MongoDB" />
+      <img className="w-20 h-20 cursor-pointer" src={mysql} alt="MySQL" />
+    </div>
+  </div>
+</section>
+
   );
 };
 
